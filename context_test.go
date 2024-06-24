ee41486a139d6c3dbf1b851359fa59c7ca3b4ea7
alfian10 <alfian.suhendar@gmail.com>
2024-06-24 14:56:52 +0700

    context with timeout

--- context_test.go
+++ context_test.go
@@ -54,6 +54,7 @@ func CreateCounter(ctx context.Context) chan int {
 			default:
 				destination <- counter
 				counter++
+				time.Sleep(1 * time.Second) //simulasi slow
 			}
 		}
 	}()
@@ -78,6 +79,26 @@ func TestCounterWithCancel(t *testing.T) {
 		}
 	}
 	cancel()
+
+	time.Sleep(2 * time.Second)
+
+	fmt.Println("Total GoRoutine :", runtime.NumGoroutine())
+}
+
+func TestCounterWithTimeout(t *testing.T) {
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+
+	parent := context.Background()
+	ctx, cancel := context.WithTimeout(parent, 5*time.Second)
+	defer cancel()
+
+	destination := CreateCounter(ctx)
+
+	fmt.Println("Total Goroutine :", runtime.NumGoroutine())
+
+	for n := range destination {
+		fmt.Println("Counter", n)
+	}
 
 	time.Sleep(2 * time.Second)
 
